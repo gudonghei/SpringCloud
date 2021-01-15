1d04aa6bb3de8df448b2fa3fc492b6a63407727a
zjh24259 <304731311@qq.com>
2021-01-15 10:51:01 +0800

    3344

---
+++ cloud-config-center-3344/src/main/java/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @ClassName: ConfigCenterMain3344
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: ZJH
+ * @Date: 2021/1/13 14:04
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
